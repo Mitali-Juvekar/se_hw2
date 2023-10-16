c23d5faf0137ca604576d223bc818c6add84df89
Mitali Prakash Juvekar <mitalijuvekar@Mitalis-MacBook-Air.local>
2023-10-16 03:09:22 -0400

    Made the transactions list immutable.

--- expense_tracker/src/model/ExpenseTrackerModel.java
+++ expense_tracker/src/model/ExpenseTrackerModel.java
@@ -2,6 +2,7 @@ package model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
 
 public class ExpenseTrackerModel {
 
@@ -20,7 +21,7 @@ public class ExpenseTrackerModel {
   }
 
   public List<Transaction> getTransactions() {
-    return transactions;
+    return Collections.unmodifiableList(transactions);
   }
 
 }
